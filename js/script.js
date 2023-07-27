764712ae72e76f389658f98c99336b921cfabea5
Steven Sawtelle <steven.sawtelle@gmail.com>
2023-07-26 23:55:27 -0700

    Fix typing

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
-document.addEventListener("input", function (event) {
-    const inputValue = event.target.value.toLowerCase();
-    const imageContainer = document.getElementById("imageContainer");
+let inputString = ""; // Variable to store the user's typed string
 
-    if (inputValue === "j") {
-        imageContainer.style.display = "block";
-    } else {
-        imageContainer.style.display = "none";
+document.addEventListener("keyup", function (event) {
+    // Get the key that was pressed
+    const keyPressed = event.key.toLowerCase();
+    console.log(keyPressed)
+    console.log(inputString)
+
+    // Check if the pressed key is a letter or a backspace
+    if (/[a-z]/.test(keyPressed) || keyPressed === "backspace") {
+        if (keyPressed === "backspace") {
+            // Remove the last character from the stored string when backspace is pressed
+            inputString = inputString.slice(0, -1);
+        } else {
+            // Append the pressed letter to the stored string
+            inputString += keyPressed;
+        }
+
+        // Check if the stored string contains "chloe"
+        if (inputString === "chloe") {
+            const imageContainer = document.getElementById("imageContainer");
+            imageContainer.style.display = "block";
+        } else {
+            const imageContainer = document.getElementById("imageContainer");
+            imageContainer.style.display = "none";
+        }
     }
 });
